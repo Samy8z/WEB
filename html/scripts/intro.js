d2924be34b80c9e45becfc322715ce88524b022b
Samy <aurelien.demabre@etud.u-picardie.fr>
2021-11-25 09:59:57 +0100

    edit

--- html/scripts/intro.js
+++ html/scripts/intro.js
@@ -1,3 +1,29 @@
+/*
 let nom = prompt("Entrez votre nom")
 alert(`Hello ${nom}`)
+*/
+const result = document.getElementById('result')
+const nomInp = document.getElementById('nom')
+const hello = document.getElementById('hello')
+const goBtn = document.getElementById('go')
+
+result.textContent = "Ceci est un test"
+
+let countClick = 0
+goBtn.addEventListener('click', function click(ev) {
+    countClick++
+    result.textContent = "Clicks : " + countClick
+})
+
+goBtn.addEventListener('click', function click(ev){
+    const valeur = nomInp.value
+    
+
+    if( !valeur || valeur.trim() === '') {
+        hello.textContent = "Saisissez un nom"
+        return
+    }
+
+    hello.innerHTML = 'Hello <b>' + valeur.trim() + '</b>'
+})
 
